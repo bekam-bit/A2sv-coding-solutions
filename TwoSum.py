e97d47073f5d170e56d0e81637183707af5300f4
Bekam Yoseph <bekamyoseph1@gmail.com>
2026-02-09 20:56:22 +0300

    Implement twoSum function to find indices of two numbers

---
+++ TwoSum.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        num_map = defaultdict(int)
+
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_map:
+                return [num_map[complement], i]
+            num_map[num] = i
+
+        return []  
+
+
+        
