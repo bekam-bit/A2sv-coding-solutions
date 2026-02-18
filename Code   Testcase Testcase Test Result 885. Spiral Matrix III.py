82a03d6c3a92fbd1d0ef58afce9010f58c70c9d1
Bekam Yoseph <bekamyoseph1@gmail.com>
2026-02-18 14:52:18 +0300

    Implement spiralMatrixIII to generate spiral matrix

---
+++ Code   Testcase Testcase Test Result 885. Spiral Matrix III.py
@@ -0,0 +1,25 @@
+class Solution:
+    def spiralMatrixIII(self, rows: int, cols: int, rStart: int, cStart: int) -> List[List[int]]:
+        res=[]
+        dir=[[0,1],[1,0],[0,-1],[-1,0]]
+
+        step = 1
+        direction = 0
+
+        while len(res) < rows * cols:
+            for _ in range(2):
+                for _ in range(step):
+                    if rStart >= 0 and rStart < rows and cStart >= 0 and cStart < cols:
+                        res.append([rStart,cStart])
+
+                    rStart += dir[direction][0]
+                    cStart += dir[direction][1]
+                
+                direction = (direction + 1) % 4
+            
+            step += 1
+        
+        return res
+
+
+        
