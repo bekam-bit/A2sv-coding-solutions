d43114b211a8cfa707ef7293fedad54e0a1bf0f2
Bekam Yoseph <bekamyoseph1@gmail.com>
2026-03-03 13:31:44 +0300

    Time: 243 ms (5.00%) | Memory: 22 MB (82.00%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.py
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.py
@@ -1,26 +1,39 @@
 class Solution:
     def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
+        cur_cnt = 0
+        left = 0
+        left2 = 0
+        cnt = Counter()
+        cnt2 = Counter()
+        total_cnt1 = 0
+        total_cnt2 = 0
 
-        def atMost(n:int)-> int:
-            cur_cnt = 0
-            left = 0
-            cnt = Counter()
-            total_cnt = 0
+        for right in range(len(nums)):
+            cnt[nums[right]] += 1
+            cnt2[nums[right]] += 1
+
+            while len(cnt) > k:
+                cnt[nums[left]] -= 1
+
+                if cnt[nums[left]] == 0:
+                    del cnt[nums[left]]
+                
+                left += 1
             
-            for right in range(len(nums)):
-                cnt[nums[right]] += 1
+            while len(cnt2) > k - 1:
+                cnt2[nums[left2]] -= 1
 
-                while left < len(nums) and len(cnt) > n:
-                    cnt[nums[left]] -= 1
-
-                    if cnt[nums[left]] == 0:
-                        del cnt[nums[left]]
-                    
-                    left += 1
+                if cnt2[nums[left2]] == 0:
+                    del cnt2[nums[left2]]
                 
-                cur_cnt = right - left + 1
-                total_cnt += cur_cnt
-                
-            return total_cnt
+                left2 += 1
+            
+            cur_cnt = right - left + 1
+            total_cnt1 += cur_cnt
+            
+            cur_cnt = right - left2 + 1
+            total_cnt2 += cur_cnt
+            
+        return total_cnt1 - total_cnt2
         
-        return atMost(k) - atMost(k-1)
+        
