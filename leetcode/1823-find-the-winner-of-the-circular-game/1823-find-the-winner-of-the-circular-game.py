5d4c10039e40301334b8fb3ed5c35d8cde85ee29
Bekam Yoseph <bekamyoseph1@gmail.com>
2026-03-21 23:45:40 +0300

    Add solution for Find the Winner of the Circular Game

---
+++ leetcode/1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        return self.helper(n, k) + 1
+
+    def helper(self, n : int, k : int) -> int:
+        if n == 1:
+            return 0
+
+        return (self.helper(n - 1, k) + k) % n
+        
+        
