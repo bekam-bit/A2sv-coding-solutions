e9f04d73de9f941c8ac2ba8eec92476cdbd7fb0f
Bekam Yoseph <bekamyoseph1@gmail.com>
2026-05-07 12:18:00 +0300

    Add solution for Employee Importance

---
+++ leetcode/0690-employee-importance/0690-employee-importance.py
@@ -0,0 +1,20 @@
+from collections import deque
+from typing import List
+
+class Solution:
+    def getImportance(self, employees: List['Employee'], id: int) -> int:
+
+        emp_map = {e.id: e for e in employees}
+
+        queue = deque([id])
+        total = 0
+
+        while queue:
+            curr_id = queue.popleft()
+            emp = emp_map[curr_id]      
+            total += emp.importance
+                 
+            for sub_id in emp.subordinates:
+                queue.append(sub_id)     
+
+        return total
