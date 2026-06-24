adbeaf2a7f11010f8db25ecff2fb6e185305b1cc
Bekam Yoseph <bekamyoseph1@gmail.com>
2026-06-24 21:28:39 +0300

    Time: 3 ms (54.59%) | Memory: 19.5 MB (45.92%) - LeetSync

--- tree/main/leetcode/692-top-k-frequent-words/top-k-frequent-words.py
+++ tree/main/leetcode/692-top-k-frequent-words/top-k-frequent-words.py
@@ -1,13 +1,14 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        res = []
         freq = Counter(words)
         max_heap = [(-cnt, word) for word, cnt in freq.items()]
-        
+
         heapq.heapify(max_heap)
-        
-        ans = []
+
         for _ in range(k):
             _, word = heapq.heappop(max_heap)
-            ans.append(word)
+            res.append(word)
+        
+        return res
 
-        return ans
