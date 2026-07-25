5f6d035015ca701f4a5438f688f23723bf66ec99
Bekam Yoseph <bekamyoseph1@gmail.com>
2026-07-25 16:30:56 +0300

    Time: 3 ms (19.62%) | Memory: 19.4 MB (41.51%) - LeetSync

---
+++ tree/main/leetcode/62-unique-paths/unique-paths.py
@@ -0,0 +1,18 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[0] * n for _ in range(m)]
+
+        for i in range(m - 1, -1, -1):
+            dp[i][n - 1] = 1
+
+        for i in range(n - 2, -1, -1):
+            dp[m - 1][i] = 1
+        
+        for r in range(m - 2, -1, -1):
+            for c in range(n - 2, -1, -1):
+                dp[r][c] = dp[r + 1][c] + dp[r][c + 1]
+        
+        return dp[0][0]
+
+
+
